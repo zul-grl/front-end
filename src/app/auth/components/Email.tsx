27f7e6d48e0e8f1c8198e82c8508cefd1e839c2c
zul-grl <zulgerel06n@gmail.com>
2025-02-20 13:47:03 +0800

    form shadcn

--- src/app/auth/components/Email.tsx
+++ src/app/auth/components/Email.tsx
@@ -1,9 +1,40 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ChevronLeft } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import Link from "next/link";
 
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, "Username must be at least 2 characters.")
+    .max(50),
+  email: z.string().email(),
+});
 const Email = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
   return (
     <div className="flex flex-col gap-5">
       <Button variant="outline" size="icon">
@@ -11,8 +42,29 @@ const Email = () => {
       </Button>
       <h3 className="font-semibold text-[24px]">Create your account</h3>
       <p className="text-[#71717A]">Sign up to explore your favorite dishes.</p>
-      <Input type="email" placeholder="Enter your email address" />
-      <Button>Let's Go</Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email address"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Let's Go</Button>
+        </form>
+      </Form>
+
       <div className="flex gap-2">
         <p className="text-[#71717A]">Already have an account?</p>
         <Link href={"/auth/login"}>
