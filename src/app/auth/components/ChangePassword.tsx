ec140953c9eb2e50c2ce8b00801503c95af672c1
zul-grl <zulgrl@Zulgerels-MacBook-Pro.local>
2025-03-24 09:29:00 +0800

    add

--- src/app/auth/components/ChangePassword.tsx
+++ src/app/auth/components/ChangePassword.tsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useUser } from "@/app/_context/UserContext";
 
 const changePasswordSchema = z.object({
   newPassword: z.string().min(8, "Password must be at least 8 characters"),
@@ -27,7 +28,7 @@ const ChangePassword = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get("id");
-
+  const { user } = useUser();
   const form = useForm<z.infer<typeof changePasswordSchema>>({
     resolver: zodResolver(changePasswordSchema),
     defaultValues: {
@@ -54,66 +55,90 @@ const ChangePassword = () => {
       );
 
       if (response) {
+        form.reset({
+          newPassword: "",
+          confirmPassword: "",
+        });
         router.push("/auth/login");
       }
     } catch (error) {
       console.error("Error password change:", error);
     }
   };
-
-  return (
-    <div className="flex flex-col gap-6 w-full max-w-md">
-      <Link href="/auth/login">
-        <Button variant="outline" size="icon">
-          <ChevronLeft />
-        </Button>
-      </Link>
-      <h3 className="text-2xl font-semibold">Change Password</h3>
-      <p className="text-[#71717A]">Enter your new password.</p>
-
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="newPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>New Password</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="New Password"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="confirmPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Confirm Password</FormLabel>
-                <FormControl>
-                  <Input
-                    type="password"
-                    placeholder="Confirm Password"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full">
-            Change Password
+  if (token) {
+    return (
+      <div className="flex flex-col gap-6 w-full max-w-md">
+        <Link href="/auth/login">
+          <Button variant="outline" size="icon">
+            <ChevronLeft />
           </Button>
-        </form>
-      </Form>
-    </div>
-  );
+        </Link>
+        <h3 className="text-2xl font-semibold">Change Password</h3>
+        <p className="text-[#71717A]">Enter your new password.</p>
+
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="newPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>New Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="New Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Confirm Password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Change Password
+            </Button>
+          </form>
+        </Form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col gap-6 w-full max-w-md">
+        <Link href="/auth/reset-password">
+          <Button variant="outline" size="icon">
+            <ChevronLeft />
+          </Button>
+        </Link>
+        <h3 className="font-semibold text-[24px]">Please verify Your Email</h3>
+        <p className="text-[#71717A]">
+          We just sent an email to {user?.email} Click the link in the email to
+          verify your account.
+        </p>
+        <Button type="submit">Resend email</Button>
+      </div>
+    );
+  }
 };
 
 export default ChangePassword;
